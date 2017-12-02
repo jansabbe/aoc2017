710f67306ea7c9905e01c4274d4d99c7d87255f0
Jan Sabbe <jan.sabbe@gmail.com>
2017-12-02 10:15:18 +0100

    Solution for day 2 part 1

--- day2/day2/Program.fs
+++ day2/day2/Program.fs
@@ -1,8 +1,37 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿
 open System
+open System.Collections.Immutable
+open System.ComponentModel.DataAnnotations.Schema
+open System.IO
+open System.Text.RegularExpressions
+open System.ComponentModel
+
+type Row = int list
+type Spreadsheet = Row list
+
+let differenceForRow (row:Row) : int =
+    (List.max row) - (List.min row)
+
+
+let checksumForSpreadsheet (spreadsheet:Spreadsheet) : int =
+    List.sumBy differenceForRow spreadsheet
+
+let stringToRow (input:string) : Row =
+    input.Split("\t")
+        |> List.ofArray
+        |> List.filter (fun s -> Regex.IsMatch(s, "^[0-9]+"))
+        |> List.map int
+
+let stringsToSpreadsheet (input: string[]) : Spreadsheet =
+    input
+        |> List.ofArray
+        |> List.map stringToRow
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let checksum =
+        File.ReadAllLines("input.txt")
+        |> stringsToSpreadsheet
+        |> checksumForSpreadsheet
+    printfn "Checksum for spreadsheet %i" checksum
     0 // return an integer exit code
--- day2/test/Tests.fs
+++ day2/test/Tests.fs
@@ -2,7 +2,50 @@ module Tests
 
 open System
 open Xunit
+open FsUnit.Xunit
+open Program
+
+let examplesForRowDifferences : obj array seq =
+    seq {
+        yield [| [1; 1];         0 |]
+        yield [| [5; 1; 9; 5];   8 |]
+        yield [| [7; 5; 3];      4 |]
+        yield [| [2; 4; 6; 8];   6 |]
+    }
+
+[<Theory; MemberData("examplesForRowDifferences")>]
+let ``Can calculate the difference between largest and smallest value in a row`` (row, expected) =
+    differenceForRow row |> should equal expected
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Can calculate the checksum by summing the differences of the rows`` () =
+    let spreadsheet = [
+        [5; 1; 9; 5];
+        [7; 5; 3];
+        [2; 4; 6; 8];
+    ]
+    checksumForSpreadsheet spreadsheet |> should equal (8 + 4 + 6)
+
+
+let examplesForTabDelimitedStrings : obj array seq =
+    seq {
+        yield [| "15";                  [15]            |]
+        yield [| "15\t23\t34\n";        [15; 23; 34]    |]
+        yield [| "";                    ([]:int list)   |]
+    }
+
+[<Theory; MemberData("examplesForTabDelimitedStrings")>]
+let ``Can convert a tab delimited string in a Row``(stringInput: string, expected: Row) =
+    stringInput
+        |> stringToRow
+        |> should equal expected
+
+
+[<Fact>]
+let ``Can convert an array of strings in a spreadsheet``() =
+    [| "15\t23\t34\n"; "3\t3";|]
+        |> stringsToSpreadsheet
+        |> should equal [
+            [15; 23; 34];
+            [3; 3]
+        ]
