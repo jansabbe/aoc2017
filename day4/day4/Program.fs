a557e5dec21011938054b85dceec6dbd447a541f
Jan Sabbe <jan.sabbe@gmail.com>
2017-12-04 20:00:44 +0100

    solution part 2 day 4

--- day4/day4/Program.fs
+++ day4/day4/Program.fs
@@ -2,20 +2,45 @@
 
 open System
 open System.IO
+open System.Threading
 
-let isInvalid (phrase:string) : bool =
-    phrase.Split(" ")
-        |> List.ofArray
+let sortLetters : string -> string = List.ofSeq >> List.map string >> List.sort >> String.concat ""
+
+let isAnagram (word1:string) (word2:string) : bool =
+    (sortLetters word1) = (sortLetters word2)
+
+let rec allCombinations list =
+  match list with
+    | [] -> []
+    | x::xs -> (List.map (fun t -> x,t) xs) @ allCombinations xs
+
+let phraseToListOfWords (phrase:string) =  phrase.Split(" ") |> List.ofArray
+
+let hasAnagram (phrase:string) : bool =
+    phrase
+        |> phraseToListOfWords
+        |> allCombinations
+        |> List.exists (fun (word1,word2) -> isAnagram word1 word2)
+
+
+let hasDoubleWord (phrase:string) : bool =
+    phrase
+        |> phraseToListOfWords
         |> List.groupBy id
         |> List.exists (fun (_,wordList) -> List.length wordList > 1)
 
-let isValid = (not << isInvalid)
+let isValid = (not << hasDoubleWord)
 
 [<EntryPoint>]
 let main argv =
-    let result =
-        File.ReadLines "input.txt"
+    let phrases = File.ReadLines "input.txt"
+    let phrasesWithoutDoubleWords =
+        phrases
             |> Seq.sumBy (fun phrase -> if isValid phrase then 1 else 0)
+    let phrasesWithoutAnagrams =
+        phrases
+            |> Seq.sumBy (fun phrase -> if not <| hasAnagram phrase then 1 else 0)
 
-    printfn "Valid phrases: %A" result
+    printfn "Phrases without double words: %A" phrasesWithoutDoubleWords
+    printfn "Phrases without anagras: %A" phrasesWithoutAnagrams
     0 // return an integer exit code
--- day4/test/Tests.fs
+++ day4/test/Tests.fs
@@ -14,3 +14,30 @@ let examplePassphrases : obj array seq = seq {
 [<Theory; MemberData("examplePassphrases")>]
 let ``in a valid passphrase words are not duplicated`` (phrase:string, expectedIsValid:bool) =
     (isValid phrase) |> should equal expectedIsValid
+
+
+let exampleAnagram : obj array seq = seq {
+    yield [| "aa"; "bb"; false |]
+    yield [| "abcde"; "ecdab"; true |]
+    yield [| "abd"; "ab"; false |]
+    yield [| "oiii"; "ioii"; true |]
+}
+
+[<Theory; MemberData("exampleAnagram")>]
+let ``can check if two words are anagram`` (word1:string, word2:string, expectedIsAnagram:bool) =
+    (isAnagram word1 word2) |> should equal expectedIsAnagram
+
+
+
+
+let exampleAnagramPhrases : obj array seq = seq {
+    yield [| "abcde fghij"; false |]
+    yield [| "abcde xyz ecdab"; true |]
+    yield [| "a ab abc abd abf abj"; false |]
+    yield [| "iiii oiii ooii oooi oooo"; false |]
+    yield [| "oiii ioii iioi iiio"; true |]
+}
+
+[<Theory; MemberData("exampleAnagramPhrases")>]
+let ``can check if pass phrase has an anagram``(phrase:string, expected:bool) =
+    hasAnagram phrase |> should equal expected
