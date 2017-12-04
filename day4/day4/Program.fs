c4593baecbe94bdbf0bc009601251eb4b2dd11c3
Jan Sabbe <jan.sabbe@gmail.com>
2017-12-04 19:25:48 +0100

    Solution part 1 day 4

--- day4/day4/Program.fs
+++ day4/day4/Program.fs
@@ -1,8 +1,21 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+
+let isInvalid (phrase:string) : bool =
+    phrase.Split(" ")
+        |> List.ofArray
+        |> List.groupBy id
+        |> List.exists (fun (_,wordList) -> List.length wordList > 1)
+
+let isValid = (not << isInvalid)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let result =
+        File.ReadLines "input.txt"
+            |> Seq.sumBy (fun phrase -> if isValid phrase then 1 else 0)
+
+    printfn "Valid phrases: %A" result
     0 // return an integer exit code
--- day4/test/Tests.fs
+++ day4/test/Tests.fs
@@ -2,7 +2,15 @@ module Tests
 
 open System
 open Xunit
+open FsUnit.Xunit
+open Program
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let examplePassphrases : obj array seq = seq {
+    yield [| "aa bb cc dd ee"; true |]
+    yield [| "aa bb cc dd aa"; false |]
+    yield [| "aa bb cc dd aaa"; true |]
+}
+
+[<Theory; MemberData("examplePassphrases")>]
+let ``in a valid passphrase words are not duplicated`` (phrase:string, expectedIsValid:bool) =
+    (isValid phrase) |> should equal expectedIsValid
