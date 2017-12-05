a6a675a7185f7965791caee040cb01b28c3ee7ce
Jan Sabbe <jan.sabbe@gmail.com>
2017-12-05 23:39:46 +0100

    Solution part1 day 5

--- day5/day5/Program.fs
+++ day5/day5/Program.fs
@@ -1,8 +1,38 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
+
+type Position = InsideList of int | OutsideList
+type JumpList = {jumps : int list; currentPosition: Position}
+
+let increaseAt (index:int) (jumps:int list) =
+    jumps
+        |> List.mapi (fun i value -> if i = index then value + 1 else value)
+
+let newPosition (jumps:int list) (index:int)  =
+    match (index + (jumps.[index])) with
+    | x when x < 0 -> OutsideList
+    | x when x >= List.length jumps -> OutsideList
+    | x -> InsideList x
+
+
+let jump ({jumps=jumps; currentPosition = position}) =
+    match position with
+    | OutsideList -> {jumps=jumps; currentPosition=position}
+    | InsideList i -> {jumps = (jumps |> increaseAt i); currentPosition=(newPosition jumps i)}
+
+let isJumpedOutside ({jumps=_; currentPosition = position}) =
+    position = OutsideList
+
+
+let jumpsNeeded jumpList =
+    Seq.unfold (fun state -> Some(state, jump state)) jumpList
+        |> Seq.findIndex isJumpedOutside
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let stack = File.ReadLines("input.txt") |> Seq.map int |> List.ofSeq
+    let result = jumpsNeeded {jumps = stack; currentPosition = InsideList 0}
+    printfn "Jumps needed: %A" result
     0 // return an integer exit code
--- day5/test/Tests.fs
+++ day5/test/Tests.fs
@@ -2,7 +2,36 @@ module Tests
 
 open System
 open Xunit
+open FsUnit.Xunit
+open Program
+
+let exampleJumps : obj array seq = seq {
+    yield [|
+        { jumps = [0;3;0;1;-3] ; currentPosition = InsideList 0 };
+        { jumps = [1;3;0;1;-3] ; currentPosition = InsideList 0 }
+    |]
+    yield [|
+        { jumps = [1;3;0;1;-3] ; currentPosition = InsideList 0 };
+        { jumps = [2;3;0;1;-3] ; currentPosition = InsideList 1 }
+    |]
+    yield [|
+        { jumps = [2;3;0;1;-3] ; currentPosition = InsideList 1 };
+        { jumps = [2;4;0;1;-3] ; currentPosition = InsideList 4 }
+    |]
+    yield [|
+        { jumps = [2;4;0;1;-3] ; currentPosition = InsideList 4 }
+        { jumps = [2;4;0;1;-2] ; currentPosition = InsideList 1 }
+    |]
+    yield [|
+        { jumps = [2;4;0;1;-2] ; currentPosition = InsideList 1 }
+        { jumps = [2;5;0;1;-2] ; currentPosition = OutsideList }
+    |]
+}
+
+[<Theory; MemberData("exampleJumps")>]
+let ``Given a jump list can generate the next jump list`` (jumpList:JumpList, expectedJumpList:JumpList) =
+    jumpList |> jump |> should equal expectedJumpList
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Given a jump list, can return how many moves needed to be outside list``() =
+    { jumps = [0;3;0;1;-3] ; currentPosition = InsideList 0 } |> jumpsNeeded |> should equal 5
